3bf680e378c0b2405fa536c0fed881cfea39d4e9
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-07-26 23:07:01 +0300

    1

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Searchbar from './components/Searchbar/Searchbar'
+// import Loader from 'react-loader-spinner';
+ //import Button from './components/Button/Button'
 // import ImageGallery from './components/ImageGallery/ImageGallery'
+import Loader from "react-loader-spinner";
 
 class App extends Component {
   state={
     hits: [],
-    // search:''
+    currentPage: 1,
+    searchQuery: '',
+    isLoading: false,
+    // error: null,
+   
   };
   //  componentDidMount(){
   //  axios.get('https://pixabay.com/api/?q=cat&page=1&key=21803950-62f4c86011510fd15fe85c0d2&image_type=photo&orientation=horizontal&per_page=12')
@@ -19,20 +26,79 @@ class App extends Component {
   //       })
   //    });
   //  }
+    componentDidUpdate(prevProps, prevState) {
+      if (prevState.searchQuery !== this.state.searchQuery) {
+        this.fetchHits()
+      }
+    }
 
      onChangeQuery = query => {
-      axios.get(`https://pixabay.com/api/?q=${query}&page=1&key=21803950-62f4c86011510fd15fe85c0d2&image_type=photo&orientation=horizontal&per_page=12`)
+       this.setState({
+         searchQuery: query,
+          currentPage: 1,
+           hits: [],
+          //  error: null,
+          });
+      //  this.fetchHits();
+      };
+
+    fetchHits = () => {
+  
+      const { currentPage, searchQuery } = this.state;
+
+      this.setState({ isLoading: true });
+    
+
+      axios
+      .get(`https://pixabay.com/api/?q=${searchQuery}&page=${currentPage}&key=21803950-62f4c86011510fd15fe85c0d2&image_type=photo&orientation=horizontal&per_page=12`, )
       .then(response => {
-        this.setState({ hits:response.data.hits,})
-      });
-      //console.log(query)
+        this.setState(prevState => ({
+          hits: [...prevState.hits, ...response.data.hits],
+          currentPage: prevState.currentPage +1,
+        }));  
+       })
+       .then(() => {
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      })
+      //  .catch(error => this.setState({ error }))
+       .finally(() => this.setState({ isLoading: false }));
+
+      //   window.scrollTo({
+      //   top: document.documentElement.scrollHeight,
+      //   behavior: 'smooth',
+   
+      // });
+      
+
+
     };
-     
+    // window.scrollTo({
+    //   top: document.documentElement.scrollHeight,
+    //   behavior: 'smooth',
+    // });
+
+
+
      render(){
 
        return (<div>
+         {/* {this.state.error && <h1>The search is empty</h1>} */}
          <Searchbar onSubmit={this.onChangeQuery}/>
        {/* <ImageGallery /> */}
+
+       {/* {loading && (
+          <Loader
+            className="Overlay"
+            // style={loaderStyles}
+            type="BallTriangle"
+            color="#00BFFF"
+            height={200}
+            width={200}
+          />
+        )} */}
 
        <ul className="ImageGallery">
     {/* <ImageGalleryItem /> */}
@@ -42,18 +108,35 @@ class App extends Component {
 </li>
 
 
+
     ))
 
 
 
     }
-    
-
-    
+        
 </ul>
+
+{this.state.isLoading && <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />}
+{/* <Button onFetchImages={this.fetchHits}/> */}
+        {this.state.hits.length > 0 && !this.state.isLoading && (
+        <button type="button" onClick={this.fetchHits}>
+          Load more</button>
+          )}
+
+
+          
+
+
        </div>
 
-       )
+       );
        
      
      }
@@ -83,5 +166,10 @@ class App extends Component {
 //     </div>
 //   );
 // }
-
+// const loaderStyles = {
+//   position: 'fixed',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%,-50%)',
+// };
 export default App;
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,17 @@
-window.scrollTo({
-    top: document.documentElement.scrollHeight,
-    behavior: 'smooth',
-  });
+import React from 'react'
+
+const Button=()=>{
+
+<button type="button" onClick={this.fetchHits}>
+          Load more</button>
+
+// window.scrollTo({
+//     top: document.documentElement.scrollHeight,
+//     behavior: 'smooth',
+//   });
+
+}
+
+
 
   export default Button;
